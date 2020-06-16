99b639dea705a24fc78a3dcb844583b8b9dd3f24
aleksandrshevchenko4 <62281789+aleksandrshevchenko4@users.noreply.github.com>
2020-06-16 11:11:06 +0300

    Update Form2.cs

--- Form2.cs
+++ Form2.cs
@@ -1,22 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Triangle_Vorm
+namespace WindowsFormsApp1
 {
-    static class Program
+    public partial class Form2 : Form
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
+
+        public Form2()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            InitializeComponent();
         }
     }
 }
